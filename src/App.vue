bf89e3eb26a99d8de4ba7319a777ac4361467c3e
zhenxi <piscesxp@outlook.com>
2023-03-04 22:19:53 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+// works fine
+import MyComponent2 from '@my-package';
+</script>
+
+<script setup lang="ts">
+// doesn't work
+import MyComponent1 from '@my-package';
+</script>
+
+<template>
+  <div></div>
+</template>
+
